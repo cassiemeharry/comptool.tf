0328d575416dbaa73f95d6bfd40fee20d4a7820a
Nick Meharry <nick@nickmeharry.com>
2013-02-12 22:09:12 -0800

    Fix AttributeError due to mismatch of gevent-websocket and gevent
    See http://stackoverflow.com/questions/9444405/gunicorn-and-websockets

--- comptool/__init__.py
+++ comptool/__init__.py
@@ -1,5 +1,16 @@
 from flask import Flask
 
+def _log_request(self):
+    log = self.server.log
+    if log:
+        if hasattr(log, 'info'):
+            log.info(self.format_request() + '\n')
+        else:
+            log.write(self.format_request() + '\n')
+
+import gevent.pywsgi
+gevent.pywsgi.WSGIHandler.log_request = _log_request
+
 app = Flask(__name__)
 app.config.from_pyfile('settings.py')
 
