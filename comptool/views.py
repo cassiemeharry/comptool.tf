fa8b57be4a3f8b9a4670b31ee5d13e9e98ce210d
Nick Meharry <nick@nickmeharry.com>
2013-04-13 17:35:31 -0700

    Reset the project. Python was a bad choice for this. Going to use Elixir instead.

--- comptool/views.py
+++
@@ -1,7 +0,0 @@
-from flask import request
-
-from comptool import app
-
-@app.route('/')
-def host_check():
-    return 'Requested from %s' % request.environ['HTTP_HOST']
--- etc/gunicorn_settings.py
+++
@@ -1,3 +0,0 @@
-bind = '0.0.0.0:80'
-user = group = 'fabric'
-worker_class = 'geventwebsocket.gunicorn.workers.GeventWebSocketWorker'
