2084838d1e7d07c1a3e7e0984aaaf441ab4ba795
Nick Meharry <nick@nickmeharry.com>
2013-08-10 18:02:07 -0700

    Log in to session

--- web/routers/application_router.ex
+++ web/routers/application_router.ex
@@ -2,17 +2,26 @@ defmodule ApplicationRouter do
   use Dynamo.Router
 
   prepare do
-    conn = conn.fetch([:cookies, :params])
+    conn = conn.fetch([:cookies, :params, :session])
     conn = conn.assign :layout, "main"
     conn.assign :top_links, []
   end
 
   forward "/api/v1", to: ApiRouter
-  forward "/casters", to: CastersRouter
+  forward "/login", to: LoginRouter
 
   get "/favicon.ico" do
     conn.resp(404, "")
   end
+
+  prepare do
+    conn = conn.assign :steam_info, get_session(conn, :steam_info)
+    conn = conn.assign :logged_in, (if conn.assigns[:steam_info], do: true, else: false)
+
+    conn
+  end
+
+  forward "/casters", to: CastersRouter
 
   get "/" do
     conn = conn.assign :section, "teams"
---
+++ web/routers/login_router.ex
@@ -0,0 +1,34 @@
+defmodule LoginRouter do
+  use Dynamo.Router
+
+  get "" do
+    web_port = Comptool.Settings.get([:web, :web_tcp_port])
+    config = [api: [root: "http://dev.comptool.tf:#{web_port}"], auth: [realm: "comptool.tf"]]
+    query_args = [
+      {"openid.ns", "http://specs.openid.net/auth/2.0"},
+      {"openid.mode", "checkid_setup"},
+      {"openid.return_to", config[:api][:root] <> "/login/return"},
+      {"openid.realm", config[:auth][:realm]},
+      # {"openid.ns.sreg", "http://openid.net/extensions/sreg/1.1"},
+      {"openid.claimed_id", "http://specs.openid.net/auth/2.0/identifier_select"},
+      {"openid.identity", "http://specs.openid.net/auth/2.0/identifier_select"},
+    ] |> URI.encode_query
+
+    redirect conn, to: "http://steamcommunity.com/openid/login?#{String.strip(query_args, ?&)}"
+  end
+
+  get "/return" do
+    claimed_id = conn.params["openid.claimed_id"] |> String.split("/") |> List.last
+    api_key = Comptool.Settings.get [:steam, :api_key]
+    query = [key: api_key, steamids: claimed_id]
+    api_endpoint = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?#{URI.encode_query query}"
+    { :ok, api_response } = HTTPotion.get(api_endpoint).body |> JSON.decode
+    api_data = api_response["response"]["players"] |> Enum.at(0)
+
+    steam_info = [steam_id: api_data["steamid"], name: api_data["personaname"], avatar: api_data["avatar"]]
+
+    conn = put_session(conn, :steam_info, steam_info)
+
+    redirect conn, to: "/"
+  end
+end
