95c9a5b7eadacec59ef8cd20a7b570ac81942a3c
Nick Meharry <nick@nickmeharry.com>
2013-08-11 20:18:39 -0700

    Hide caster tool behind flag

--- web/routers/casters_router.ex
+++ web/routers/casters_router.ex
@@ -3,7 +3,28 @@ defmodule CastersRouter do
 
   prepare do
     conn = conn.assign :section, "casters"
-    conn.assign :top_links, ["options"]
+
+    if conn.assigns[:logged_in] do
+      user = Comptool.DB.User.get_by_steam_id! conn.assigns[:steam_info][:steam_id]
+      if ! user.caster_access do
+        case { user.caster_access_requested, conn.path, conn.method } do
+          { false, "/casters/request-access", "POST" } ->
+            with_flag = user.update(caster_access_requested: true)
+            IO.puts "Requesting access for steam id #{with_flag.name} (#{with_flag.steam_id})"
+            Comptool.DB.User.save(with_flag)
+            redirect! conn, to: "/casters/access-requested"
+          { true, _, _ } ->
+            halt! render(conn, "casters/access_requested.html")
+          _ ->
+            halt! render(conn, "casters/request_access.html")
+        end
+      else
+        conn = conn.assign :top_links, ["options"]
+        conn.assign :user, user
+      end
+    else
+      redirect! conn, "/login"
+    end
   end
 
   get "" do
